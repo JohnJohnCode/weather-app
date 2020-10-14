abe83d41e9de1d3ddc861cf63811481cc01a4504
JohnJohnCode <jsynthlips@gmail.com>
2020-10-14 18:21:06 +0200

    Updates

---
+++ precache-manifest.8168a551b0800bbfbf55d5f73199891d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e12a45791881f67deb2b9112626732",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e1cb46f1357d7e716056",
+    "url": "/weather-app/static/css/main.d4450d85.chunk.css"
+  },
+  {
+    "revision": "a87974c5dec55b27405d",
+    "url": "/weather-app/static/js/2.60476aae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.60476aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1cb46f1357d7e716056",
+    "url": "/weather-app/static/js/main.f871e560.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
